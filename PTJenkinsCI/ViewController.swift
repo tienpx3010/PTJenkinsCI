644cb1cf2713cd2944ce59c968bc9f6fb8aa1d20
tienpx3010 <tienpx.x@gmail.com>
2019-06-12 15:28:05 +0700

    Initial Commit

---
+++ PTJenkinsCI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PTJenkinsCI
+//
+//  Created by Phạm Xuân Tiến on 6/12/19.
+//  Copyright © 2019 Sun*. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
